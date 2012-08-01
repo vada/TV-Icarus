5494963f72e67e77e386addc1bc3a5c2ae0472f0
TuxLyn <tuxlyn@gmail.com>
2012-07-31 19:49:59 -0600

    fixed link

--- pages/footer.php
+++ pages/footer.php
@@ -2,8 +2,7 @@
   </article>
   <footer>
     <!--// Please do not remove this footer, thank you. //-->
-    <p> TV-Icarus Coded by TuxLyn from <a href="http://gotux.net/">GoTux.net</a> 
-    &bull; Powered by <a href="http://www.tvrage.com/">TVRage.com</a> </p>
+    <p> Powered by <a href="http://www.tvrage.com/">TVRage.com</a> </p>
   </footer>
 </div> <!--#page-->
 </div> <!--#wrapper-->
